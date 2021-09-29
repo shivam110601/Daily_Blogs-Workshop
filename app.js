c52f0855c201f5361dede8f75c4329c36785cd68
Shivam <shivam.m.110601@gmail.com>
2021-09-29 23:08:02 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+
+const blogs = [];
+
+let theDate = new Date();
+// let month = theDate.getMonth();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/compose",function(req,res){
+    res.render("compose");
+});
+
+app.get("/blogs",function(req,res){
+    res.render("blogs",{blogs:blogs});
+});
+
+
+app.post("/compose",function(req,res){
+    const blog = {
+        title: req.body.blogTitle,
+        content: req.body.blogBody,
+        time: theDate.toLocaleDateString()
+    }
+
+
+    blogs.push(blog);
+
+
+    res.redirect("/blogs");
+});
+
+
+
+
+
+app.listen(3000,function(){
+    console.log('server is running in port 3000')
+})
