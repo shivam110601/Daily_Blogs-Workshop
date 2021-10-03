6934a8ed1852b59b8598346bd0c334a0d3ea86da
Shivam <shivam.m.110601@gmail.com>
2021-10-03 09:29:21 +0530

    third commit

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.post("/compose",function(req,res){
 
 
 
-app.listen(3000,function(){
-    console.log('server is running in port 3000')
+app.listen(process.env.PORT || 3000,function(){
+    console.log('server is running')
 })
